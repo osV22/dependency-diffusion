d393b7b178988aade8d36818b4aa4c093bf39fba
osv22 <ghalib@usc.edu>
2021-04-26 07:23:39 -0400

    logging near completion, refactoring

--- checkPackage.sh
+++ checkPackage.sh
@@ -5,6 +5,11 @@
 ghUsername=""
 ghAPItoken=""
  
+reason=""
+warning_given=""
+action_taken=""
+ 
+ 
 while getopts "p:a:s:o:" arg; do #Later add v: for verbose
    case $arg in
       p) packageName=$OPTARG;;
@@ -21,9 +26,9 @@ echo "Output Loc: $outputLoc"
 echo "Age Min: $minAge"
 echo "Stars Min: $minStars"
  
- 
 # Get Page from PyPI | silent so user does not notice anything
-packagePage=$(curl -s https://pypi.org/project/$packageName/ | grep "github.com/repos/") 
+pypiUrl="https://pypi.org/project/$packageName/"
+packagePage=$(curl -s $pypiUrl | grep "github.com/repos/") 
  
 # Check if there is a valid repo:
 if [[ $packagePage != *"github.com/repos/"* ]]; then
@@ -38,10 +43,11 @@ author=$(echo $packagePage | cut -f5 -d/)
 echo "Author is: $author"
  
 # GitHub Repo Page
+ghRepoUrl="https://api.github.com/repos/$author/$packageName"
 ghRepoInfo=$(exec 
    curl -s \
       -H "Accept: application.vnd.github.v3+json" \
-      -u $ghUsername:$ghAPItoken https://api.github.com/repos/$author/$packageName
+      -u $ghUsername:$ghAPItoken $ghRepoUrl
 )
  
 #moved=$(echo $ghRepoInfo | grep "Moved Permanently")
@@ -64,18 +70,8 @@ fi
 #echo $ghRepoInfo
  
 # Stars Num
-declare -i starCount=$(echo $ghRepoInfo | grep -o "stargazers_count\": .*" | awk '{print $2}' | tr -d '",:' ) # I guess I'm into self-harm at this point
-echo "Total Stars: $starCount"
- 
-if [[ $starCount -lt $minStars ]]; 
-then
-   echo "Not STARRED ENOUGH! FAILED"
-   printf "\nstar count: $starCount\nmin stars req: $minStars\n\n"
- 
-else
-   echo "STARRED! PASSED"
-   printf "\nstar count: $starCount\nmin stars req: $minStars\n\n"
-fi
+declare -i repoStars=$(echo $ghRepoInfo | grep -o "stargazers_count\": .*" | awk '{print $2}' | tr -d '",:' ) # I guess I'm into self-harm at this point
+echo "Total Stars: $repoStars"
  
  
 # Get Repo Age
@@ -87,42 +83,63 @@ echo "Created on: $createdAt"
 repoAge=$(( ($(date -d "$currentDate" +%s) - $(date -d "$createdAt" +%s)) / (60*60*24) ))
 echo "Repo age: $repoAge days old"
  
-if [[ $repoAge -lt $minAge ]]; 
-then
-   echo "Repo is very NEW! FAILED"
+ 
+logEntry() {
+ 
+   local warningNote="[!] INSTALL WARNING: The \"$packageName\" package you tried installing does not meet the minimum repository $1 requirement set by your administrator."
+   local actionNote="[+] To protect you from a possibly malicious/ misspelled packages, the incident will be logged and package uninstalled"
+ 
+   local id=$(echo $(grep -o "}," log.json | wc -l)+1 | bc) # CHANGE TO CORRECT FILE LATER
+   local date=$(date --iso-8601=seconds)
+   local package=$packageName
+   local pip_version=3
+   local user=$(whoami)
+   local warning_given=$warningNote
+   local action_taken=$actionNote
+   local reason_given=$reason
  
  
-else
-   echo "Package is OLD enough! PASSED"
-   # TRIGGER a log entry
-   id=1
-   date="12/12/12"
-   package="matplotlib"
-   pip_version=3
-   user="x"
-   reason="Package only has x stars, which is y less than the minimum set in the diffusion-preexec function."
-   warning_given="Package is too new and has been uninstalled."
-   action_taken="Uninstalled"
+   if [[ $1 == "stars" ]]; then
+      reason="[+] Repository star count: $repoStars stars... Minimum stars required: $minStars"
+   elif [[ $1 == "age" ]]; then
+      reason="[+] Repository age: $repoAge days old... Minimum age required: $minStars"
+   else
+      #! TODO: Finish this part and move on to uninstall process
+      echo wow both not met!!
+   fi
+ 
+   echo -e "\n$warningNote\n$actionNote\n$reason\n"
  
  
    jsonTemp='
       {
          "id":"%s",
          "date":"%s",
-         "package":"%s", # change to array later on 
+         "package":"%s", 
+         "pypi_url":"%s", 
+         "github_url":"%s", 
          "pip_version":"%s",
          "user":"%s",
          "reason":"%s",
          "warning_given":"%s",
          "action_taken":"%s"
       }'
-
-   #! TODO: Wrap in an if-else and insert arr start if empty, add actual vars, 
-   report=$(printf "$jsonTemp" "$id" "$date" "$package" "$pip_version" "$user" "$reason" "$warning_given" "$action_taken")
+   report=$(printf "$jsonTemp" "$id" "$date" "$package" "$pypiUrl" "$ghRepoUrl" "$pip_version" "$user" "$reason" "$warning_given" "$action_taken")
    sed -i '$ d' log.json
    echo -e "$report,\n]" >> log.json # b/c json_pp does not do tabs/ non-json formats
  
    # For regular json. 
    #echo "$jsonTemp" | tr -d '\n' | tr -d ' '
+}
  
+ 
+if [[ $repoStars -lt $minStars ]] && [[ $repoAge -lt $minAge ]]; then
+   logEntry "age and star count"
+elif [[ $repoAge -lt $minAge ]]; then
+   logEntry "age"
+elif [[ $repoStars -lt $minStars ]]; then
+   logEntry "stars" 
+else
+   echo "STARRED! PASSED"
+   printf "\nstar count: $repoStars\nmin stars req: $minStars\n\n"
 fi
